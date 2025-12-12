0f451ae0ab036a66988da98d868d9decb45a56f1
DudaPereira <maria.epereira@g.globo>
2025-12-12 17:43:36 -0300

    fix: ajusta configuracao CORS

--- backend/trancascore-app/src/main/java/com/pereira/trancascore_app/config/CorsConfig.java
+++ backend/trancascore-app/src/main/java/com/pereira/trancascore_app/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(
+                .allowedOrigins(
                         "http://localhost:4200",
                         "https://trancascore-app.vercel.app"
                 )
